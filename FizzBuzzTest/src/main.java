4aacfcbe7b8fabb96650036b8af9a51607074a73
Mitri Syriani <mitri.syriani@razer.zone>
2017-07-05 14:38:18 -0700

    Added some comments to the code

--- FizzBuzzTest/src/main.java
+++ FizzBuzzTest/src/main.java
@@ -4,18 +4,22 @@ public class main {
 	public static void main(String[] args)
 	{
 		for(int i = 1; i <= 100; i++)
-		{
+		{   
+			//Checks to see if i is multiple of 3 and 5
 			if(i%3 == 0 && i%5 == 0){
 				System.out.println("FizzBuzz");
 			}
+			//multiple of 3
 			else if(i%3 == 0)
 			{
 				System.out.println("Fizz");
 			}
+			//multiple of 5
 			else if(i%5 == 0)
 			{
 				System.out.println("Buzz");
 			}
+			//If not multiple of 3 or 5
 			else
 			{
 				System.out.println(i);
